bb42839cfaefe782d1be3deffb75b256c1ae2c67
DESKTOP-9E6FBSF\Alphapay001 <meng8859@gmail.com>
2017-11-29 17:10:37 -0800

    should fix PutJsonCurl function

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 var FlashPayDataBase = require('../lib/data');
 var FlashPayUnifiedOrder = require('../lib/data');
 var FlashPayApi = require('../lib/api');
@@ -10,35 +11,33 @@ var CREDENTIAL_CODE =require('../lib/config').CREDENTIAL_CODE
 process.env.TZ = 'America/Vancouver';
 
 
+
 var p = new FlashPayApi;
 // console.log("p.getMillisecond():" + p.getMillisecond());
 
 
-var s = new FlashPayDataBase;
-s.setTime('123');
-s.setNonceStr('string');
-s.setSign('abc');
-console.log("s.gettime():" + s.getTime());
-console.log("s.getNonceStr():" + s.getNonceStr());
-console.log("s.toSignParams():" + s.toSignParams());
-console.log("s.makeSign():" + s.setSign());
-console.log("s.toBodyParams():" + s.toBodyParams());
-console.log("s.toQueryParams():" + s.toQueryParams());
+// var s = new FlashPayDataBase;
+// s.setTime('123');
+// s.setNonceStr('string');
+// s.setSign('abc');
+// console.log("s.gettime():" + s.getTime());
+// console.log("s.getNonceStr():" + s.getNonceStr());
+// console.log("s.toSignParams():" + s.toSignParams());
+// console.log("s.makeSign():" + s.setSign());
+// console.log("s.toBodyParams():" + s.toBodyParams());
+// console.log("s.toQueryParams():" + s.toQueryParams());
 
-var w = new FlashPayUnifiedOrder;
-var time = new Date();
-      // .toISOString().
-      // replace(/T/, ' ').      // replace T with a space
-      // replace(/\..+/, '')     // delete the dot and everything after
-w.setOrderId(PARTNER_CODE + time);
-w.setDescription("test");
-w.setPrice("100");
-w.setCurrency("CAD");
-w.setNotifyUrl("https://www.flashpayment.com/notify_url");
-w.setOperator("123456");
+// var w = new FlashPayUnifiedOrder;
+// var time = moment.utc();
+// w.setOrderId(PARTNER_CODE + time);
+// w.setDescription("test");
+// w.setPrice("100");
+// w.setCurrency("CAD");
+// w.setNotifyUrl("https://www.flashpayment.com/notify_url");
+// w.setOperator("123456");
 
-var result = p.qrOrder(w);
-console.log("result :" + result);
+// var result = p.qrOrder(w);
+// console.log("result :" + result);
 // s.setOrderId9(s.partner_code);
 
 
--- routes/qr.js
+++ routes/qr.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const util = require('util'); // show object
+
 
 var PARTNER_CODE =require('../lib/config').PARTNER_CODE;  //get PARTNER_CODE and CREDENTIAL_CODE
 var CREDENTIAL_CODE =require('../lib/config').CREDENTIAL_CODE
 process.env.TZ = 'America/Vancouver';
 
+
 var FlashPayApi = require('../lib/api');
 var FlashPayUnifiedOrder = require('../lib/data');
 
 var p = new FlashPayApi;
+// console.log("p.getMillisecond():" + p.getMillisecond());
 
 var w = new FlashPayUnifiedOrder;
-// var q = new FlashPayResults;
-
 var time = new Date();
-      // .toISOString().
-      // replace(/T/, ' ').      // replace T with a space
-      // replace(/\..+/, '')     // delete the dot and everything after
 w.setOrderId(PARTNER_CODE + time);
 w.setDescription("test");
 w.setPrice("100");
@@ -24,8 +23,11 @@ w.setCurrency("CAD");
 w.setNotifyUrl("https://www.flashpayment.com/notify_url");
 w.setOperator("123456");
 
+
 var result = p.qrOrder(w);
-console.log("result :" + result);
+// console.log("p :" + util.inspect(p, false, null));
+// console.log("w :" + util.inspect(w, false, null));
+console.log("result :" + util.inspect(result, false, null));
 // s.setOrderId(s.partner_code);
 
 /* GET qr. */
